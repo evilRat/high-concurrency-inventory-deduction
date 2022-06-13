76ac615f4d908ae991e6cb5b43e2c194da5c9687
kongzheng1993 <kongzheng1993@hotmail.com>
2022-06-13 23:24:43 +0800

    init

---
+++ src/test/java/com/evil/highconcurrentinventorydeduction/HighConcurrentInventoryDeductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.highconcurrentinventorydeduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrentInventoryDeductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
